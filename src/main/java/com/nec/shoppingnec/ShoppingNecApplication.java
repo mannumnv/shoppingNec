eef6c5ec4dd20ae2d4510e92af656164f2ddf520
mannu <mannumnv67@gmail.com>
2025-11-13 23:40:25 +0900

    initial commit

---
+++ src/main/java/com/nec/shoppingnec/ShoppingNecApplication.java
@@ -0,0 +1,15 @@
+package com.nec.shoppingnec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingNecApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(ShoppingNecApplication.class, args);
+        System.out.println("ShoppingNecApplication started successfully.");
+	}
+
+}
